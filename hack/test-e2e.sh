4dbed15ee8dc43f3ef883f3c0903f524278d0e45
Adam Kaplan <adam.kaplan@redhat.com>
2021-08-05 13:42:17 -0400

    fix e2e script
    - Fix paths to go files and manifests
    - Fix conditional to deploy e2e scripts

--- hack/test-e2e.sh
+++ hack/test-e2e.sh
@@ -10,7 +10,6 @@ set -o pipefail
 # TEST_TIMEOUT: timeout for tests. Defauls to 30m, can be any parsable duration.
 # TEST_SKIP_DEPLOY: if true, skip deployment of the driver and its prerequisites (namespace, service account, etc.)
 
-BASE_DIR=$(dirname "$0")
 suite=${TEST_SUITE:-"normal"}
 timeout=${TEST_TIMEOUT:-"30m"}
 skipDeploy=${TEST_SKIP_DEPLOY:-"false"}
@@ -22,20 +21,20 @@ function run () {
 
 function deploy() {
     echo "Deploying CSI driver prerequisites"
-    run oc apply -f "${BASE_DIR}/0000_10_projectedresource.crd.yaml"
-    run oc apply -f "${BASE_DIR}/00-namespace.yaml"
-    run oc apply -f "${BASE_DIR}/01-service-account.yaml"
-    run oc apply -f "${BASE_DIR}/02-cluster-role.yaml"
-    run oc apply -f "${BASE_DIR}/03-cluster-role-binding.yaml"
-    run oc apply -f "${BASE_DIR}/csi-hostpath-driverinfo.yaml"
+    run oc apply -f "deploy/0000_10_projectedresource.crd.yaml"
+    run oc apply -f "deploy/00-namespace.yaml"
+    run oc apply -f "deploy/01-service-account.yaml"
+    run oc apply -f "deploy/02-cluster-role.yaml"
+    run oc apply -f "deploy/03-cluster-role-binding.yaml"
+    run oc apply -f "deploy/csi-hostpath-driverinfo.yaml"
 }
 
 function test() {
     echo "Running e2e suite ${suite} with SKIP_DEPLOY=${skipDeploy}."
-    KUBERNETES_CONFIG=${KUBECONFIG} SKIP_DEPLOY=${skipDeploy} go test -race -count 1 -tags "${suite}" -timeout "${timeout}" -v "${BASE_DIR}/test/e2e/..."
+    KUBERNETES_CONFIG=${KUBECONFIG} SKIP_DEPLOY=${skipDeploy} go test -race -count 1 -tags "${suite}" -timeout "${timeout}" -v "./test/e2e/..."
 }
 
-if [[ ! ${skipDeploy} ]]; then {
+if [[ ${skipDeploy} == "false" ]]; then {
     deploy
 } fi
 
