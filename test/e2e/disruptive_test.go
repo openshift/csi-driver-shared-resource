15ec3800385ac1e42c5be412e07b0e6a9e1b42f4
Akram Ben Aissi <akram.benaissi@gmail.com>
2021-10-14 19:32:08 +0100

    Add read-only mount in disruptive test

--- test/e2e/disruptive_test.go
+++ test/e2e/disruptive_test.go
@@ -30,3 +30,27 @@ func TestBasicThenDriverRestartThenChangeShare(t *testing.T) {
 	testArgs.SearchString = "ca.crt"
 	framework.ExecPod(testArgs)
 }
+
+
+func TestBasicThenDriverRestartThenChangeShareWithReadOnlyMount(t *testing.T) {
+	testArgs := &framework.TestArgs{
+		T: t,
+	}
+	testArgs.ReadOnly = true
+	prep(testArgs)
+	framework.CreateTestNamespace(testArgs)
+	defer framework.CleanupTestNamespaceAndClusterScopedResources(testArgs)
+	basicShareSetupAndVerification(testArgs)
+
+	t.Logf("%s: initiating csi driver restart", time.Now().String())
+	framework.RestartDaemonSet(testArgs)
+	t.Logf("%s: csi driver restart complete, check test pod", time.Now().String())
+	testArgs.TestDuration = 30 * time.Second
+	testArgs.SearchString = "invoker"
+	framework.ExecPod(testArgs)
+
+	t.Logf("%s: now changing share", time.Now().String())
+	framework.ChangeShare(testArgs)
+	testArgs.SearchString = "ca.crt"
+	framework.ExecPod(testArgs)
+}
