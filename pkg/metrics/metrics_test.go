91eb623eb030374531457a6f06fad8a73208a58c
Alice Rum <alice.rum@redhat.com>
2021-10-18 13:19:15 +0200

    Mount metrics exposure, new monitoring objects
    This commit adds mount metrics exposure (/metrics endpoint and mounts metrics
    collector) for the openshift_csi_share_mount_total metrics.
    Also included are new deployment objects for the prometheus operator, so
    that this new metric is scraped correctly by OpenShift.

---
+++ pkg/metrics/metrics_test.go
@@ -0,0 +1,95 @@
+package metrics
+
+import (
+	"bytes"
+	"net/http"
+	"strings"
+	"testing"
+
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+)
+
+type fakeResponseWriter struct {
+	bytes.Buffer
+	statusCode int
+	header     http.Header
+}
+
+func (f *fakeResponseWriter) Header() http.Header {
+	return f.header
+}
+
+func (f *fakeResponseWriter) WriteHeader(statusCode int) {
+	f.statusCode = statusCode
+}
+
+func TestMetrics(t *testing.T) {
+	for _, test := range []struct {
+		name        string
+		expected    []string
+		notExpected []string
+		mounts      map[bool]int
+	}{
+		{
+			name: "One true, two false",
+			expected: []string{
+				`# TYPE openshift_csi_share_mount_total counter`,
+				`openshift_csi_share_mount_total{succeeded="false"} 2`,
+				`openshift_csi_share_mount_total{succeeded="true"} 1`,
+			},
+			mounts:      map[bool]int{true: 1, false: 2},
+			notExpected: []string{},
+		},
+		{
+			name: "Two true, no false",
+			expected: []string{
+				`# TYPE openshift_csi_share_mount_total counter`,
+				`openshift_csi_share_mount_total{succeeded="true"} 2`,
+			},
+			notExpected: []string{
+				`openshift_csi_share_mount_total{succeeded="false"}`,
+			},
+			mounts: map[bool]int{true: 2},
+		},
+		{
+			name: "No true, three false",
+			expected: []string{
+				`# TYPE openshift_csi_share_mount_total counter`,
+				`openshift_csi_share_mount_total{succeeded="false"} 3`,
+			},
+			notExpected: []string{
+				`openshift_csi_share_mount_total{succeeded="true"}`,
+			},
+			mounts: map[bool]int{false: 3},
+		},
+	} {
+		registry := prometheus.NewRegistry()
+		mountCounter = createMountCounter()
+		registry.MustRegister(mountCounter)
+
+		for k, v := range test.mounts {
+			for i := 0; i < v; i += 1 {
+				IncMountCounter(k)
+			}
+		}
+
+		h := promhttp.HandlerFor(registry, promhttp.HandlerOpts{ErrorHandling: promhttp.PanicOnError})
+		rw := &fakeResponseWriter{header: http.Header{}}
+		h.ServeHTTP(rw, &http.Request{})
+
+		respStr := rw.String()
+
+		for _, s := range test.expected {
+			if !strings.Contains(respStr, s) {
+				t.Errorf("testcase %s: expected string %s did not appear in %s", test.name, s, respStr)
+			}
+		}
+
+		for _, s := range test.notExpected {
+			if strings.Contains(respStr, s) {
+				t.Errorf("testcase %s: expected to not find string %s in %s", test.name, s, respStr)
+			}
+		}
+	}
+}
---
+++ pkg/metrics/server.go
@@ -0,0 +1,53 @@
+package metrics
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"k8s.io/klog/v2"
+)
+
+// BuildServer creates the http.Server struct
+func BuildServer(port int) (*http.Server, error) {
+	if port <= 0 {
+		klog.Error("invalid port for metric server")
+		return nil, errors.New("invalid port for metrics server")
+	}
+
+	bindAddr := fmt.Sprintf(":%d", port)
+	router := http.NewServeMux()
+	router.Handle("/metrics", promhttp.Handler())
+	srv := &http.Server{
+		Addr:    bindAddr,
+		Handler: router,
+	}
+
+	return srv, nil
+}
+
+// StopServer stops the metrics server
+func StopServer(srv *http.Server) {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err := srv.Shutdown(ctx); err != nil {
+		klog.Warningf("Problem shutting down HTTP server: %v", err)
+	}
+}
+
+// RunServer starts the metrics server.
+func RunServer(srv *http.Server, stopCh <-chan struct{}) {
+	go func() {
+		err := srv.ListenAndServe()
+		if err != nil && err != http.ErrServerClosed {
+			klog.Errorf("error starting metrics server: %v", err)
+		}
+	}()
+	<-stopCh
+	if err := srv.Close(); err != nil {
+		klog.Errorf("error closing metrics server: %v", err)
+	}
+}
