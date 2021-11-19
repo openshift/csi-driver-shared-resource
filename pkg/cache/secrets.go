a6564259cd25765087a7d6af341a73f571122f0f
gabemontero <gmontero@redhat.com>
2021-11-19 17:28:14 -0500

    reduce 'objcache' to just callback maps, leverage new 'combo client'

--- pkg/cache/secrets.go
+++ pkg/cache/secrets.go
@@ -1,15 +1,13 @@
 package cache
 
 import (
-	"context"
 	"sync"
 
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/client-go/kubernetes"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/labels"
 	"k8s.io/klog/v2"
 
-	sharev1alpha1 "github.com/openshift/api/sharedresource/v1alpha1"
-	corev1 "k8s.io/api/core/v1"
+	"github.com/openshift/csi-driver-shared-resource/pkg/client"
 )
 
 /*
@@ -27,10 +25,6 @@ On the use of sync.Map, see the comments in share.go
 */
 
 var (
-	// secrets is our global configmap id (namespace + name) to secret map, where entries are populated from
-	// controller events; it serves to facilitate quick lookup during share event processing, when the share references
-	// a secret
-	secrets = sync.Map{}
 	// secretUpsertCallbacks has a key of the CSI volume ID and a value of the function to be called when a given
 	// secret is updated, assuming the driver has mounted a share CSI volume with the configmap in a pod somewhere, and
 	// the corresponding storage on the pod gets updated by the function that is the value of the entry.  Otherwise,
@@ -40,72 +34,28 @@ var (
 	// same thing as secretUpsertCallbacks but deletion of secrets, and of of course the controller relist does not
 	// come into play here.
 	secretDeleteCallbacks = sync.Map{}
-	// secretsWithShare is a filtered list of secrets where, via share events, we know at least one active share references
-	// a given secret; when possible we range over this list vs. secrets
-	secretsWithShare = sync.Map{}
-	// sharesWaitingOnSecrets conversely is for when a share has been created that references a secret, but that
-	// secret has not been recognized by the controller; quite possibly timing events on when we learn of sharedSecrets
-	// and secret if they happen to be created at roughly the same time come into play; also, if a pod with a share
-	// pointing to a secret has been provisioned, but the the csi driver daemonset has been restarted, such timing
-	// of events where we learn of sharedSecrets before their secrets can also occur, as we attempt to rebuild the CSI driver's
-	// state
-	sharesWaitingOnSecrets = sync.Map{}
 )
-
-// GetSecret retrieves a secret from the list of secrets referenced by SharedSecrets
-func GetSecret(key interface{}) *corev1.Secret {
-	obj, loaded := secretsWithShare.Load(key)
-	if loaded {
-		s, _ := obj.(*corev1.Secret)
-		return s
-	}
-	return nil
-}
-
-// SetSecret based on the shared-data-key, which contains the resource's namespace and name, this
-// method can fetch and store it on cache.  This method is called when the controller is not watching
-// secrets, and the CSI driver must retrieve the secret when processing a NodePublishVolume call
-// from the kubelet.
-func SetSecret(kubeClient kubernetes.Interface, sharedDataKey string) error {
-	ns, name, err := SplitKey(sharedDataKey)
-	if err != nil {
-		return err
-	}
-
-	secret, err := kubeClient.CoreV1().Secrets(ns).Get(context.TODO(), name, metav1.GetOptions{})
-	if err != nil {
-		return err
-	}
-
-	UpsertSecret(secret)
-	return nil
-}
 
 // UpsertSecret adds or updates as needed the secret to our various maps for correlating with SharedSecrets and
 // calls registered upsert callbacks
 func UpsertSecret(secret *corev1.Secret) {
 	key := GetKey(secret)
 	klog.V(6).Infof("UpsertSecret key %s", key)
-	secrets.Store(key, secret)
-	// in case share arrived before secret
-	processedSharesWithoutSecrets := []string{}
-	sharesWaitingOnSecrets.Range(func(key, value interface{}) bool {
-		shareKey := key.(string)
-		share := value.(*sharev1alpha1.SharedSecret)
-		br := share.Spec.SecretRef
-		secretKey := BuildKey(br.Namespace, br.Name)
-		secretsWithShare.Store(secretKey, secret)
-		//NOTE: share update ranger will store share in sharedSecrets sync.Map
-		// and we are supplying only this specific share to the csi driver update range callbacks.
-		shareSecretsUpdateCallbacks.Range(buildRanger(buildCallbackMap(share.Name, share)))
-		processedSharesWithoutSecrets = append(processedSharesWithoutSecrets, shareKey)
-		return true
-	})
-	for _, shareKey := range processedSharesWithoutSecrets {
-		sharesWaitingOnSecrets.Delete(shareKey)
+	// first, find the shares pointing to this secret, and call the callbacks, in case certain pods
+	// have had their permissions revoked; this will also handle if we had share events arrive before
+	// the corresponding secret
+	sharedSecretsList, err := client.GetListers().SharedSecrets.List(labels.Everything())
+	if err == nil {
+		for _, share := range sharedSecretsList {
+			if share.Spec.SecretRef.Namespace == secret.Namespace && share.Spec.SecretRef.Name == secret.Name {
+				shareSecretsUpdateCallbacks.Range(buildRanger(buildCallbackMap(share.Name, share)))
+			}
+		}
+	} else {
+		klog.Warningf("error during UpsertSecret on shared secrets lister list: %s", err.Error())
 	}
+
 	// otherwise process any share that arrived after the secret
-	secretsWithShare.Store(key, secret)
 	secretUpsertCallbacks.Range(buildRanger(buildCallbackMap(key, secret)))
 }
 
@@ -113,20 +63,21 @@ func UpsertSecret(secret *corev1.Secret) {
 func DelSecret(secret *corev1.Secret) {
 	key := GetKey(secret)
 	klog.V(4).Infof("DelSecret key %s", key)
-	secrets.Delete(key)
 	secretDeleteCallbacks.Range(buildRanger(buildCallbackMap(key, secret)))
-	secretsWithShare.Delete(key)
 }
 
 // RegisterSecretUpsertCallback will be called as part of the kubelet sending a mount CSI volume request for a pod;
 // if the corresponding share references a secret, then the function registered here will be called to possibly change
 // storage
-func RegisterSecretUpsertCallback(volID string, f func(key, value interface{}) bool) {
+func RegisterSecretUpsertCallback(volID, sID string, f func(key, value interface{}) bool) {
 	secretUpsertCallbacks.Store(volID, f)
-	// cycle through the secrets with sharedSecrets, where if the share associated with the volID CSI volume mount references
-	// one of the secrets provided by the Range, the storage of the corresponding data on the pod will be completed using
-	// the supplied function
-	secretsWithShare.Range(f)
+	ns, name, _ := SplitKey(sID)
+	s := client.GetSecret(ns, name)
+	if s != nil {
+		f(BuildKey(s.Namespace, s.Name), s)
+	} else {
+		klog.Warningf("not found on secret with key %s vol %s", sID, volID)
+	}
 }
 
 // UnregisterSecretUpsertCallback will be called as part of the kubelet sending a delete CSI volume request for a pod
--- pkg/cache/shares.go
+++ pkg/cache/shares.go
@@ -1,11 +1,10 @@
 package cache
 
 import (
+	"github.com/openshift/csi-driver-shared-resource/pkg/client"
 	"sync"
 
 	"k8s.io/klog/v2"
-
-	corev1 "k8s.io/api/core/v1"
 
 	sharev1alpha1 "github.com/openshift/api/sharedresource/v1alpha1"
 )
@@ -43,10 +42,6 @@ variety is how we currently verify that a given pod is *STILL* allowed to access
 */
 
 var (
-	// shareConfigMaps/shareSecrets is our global share ID (remember it is cluster scoped) to share map; generally, it facilitates lookup
-	// when we are dealing with Pod creation events from the kubelet
-	shareConfigMaps = sync.Map{}
-	shareSecrets    = sync.Map{}
 	// sharesUpdateCallbacks/shareSecretsUpdateCallbacks have a key of the CSI volume ID and a value of the function to be called when a given
 	// share is to updated, assuming the driver has mounted a share CSI volume in a pod somewhere.  Otherwise,
 	// this map is empty and share updates result in a no-op.  This map is used both when we get an event for a given
@@ -63,19 +58,15 @@ var (
 func AddSharedConfigMap(share *sharev1alpha1.SharedConfigMap) {
 	br := share.Spec.ConfigMapRef
 	key := BuildKey(br.Namespace, br.Name)
-	klog.V(4).Infof("AddSharedConfigMap key %s", key)
-	obj, ok := configmaps.Load(key)
-	if obj != nil && ok {
-		cm := obj.(*corev1.ConfigMap)
-		configmapsWithShares.Store(key, cm)
+	klog.V(4).Infof("AddSharedConfigMap share %s key %s", share.Name, key)
+	cm := client.GetConfigMap(br.Namespace, br.Name)
+	if cm != nil {
 		// so this line build a map with a single entry, the share from this event, and then
 		// applies the function(s) supplied by the CSI volume code in order to make changes based
 		// on this event
+		klog.V(4).Infof("AddSharedConfigMap share %s key %s start range", share.Name, key)
 		shareConfigMapsUpdateCallbacks.Range(buildRanger(buildCallbackMap(share.Name, share)))
-		//NOTE we do not store share in shareConfigMaps unless the backing resource is available
-		shareConfigMaps.Store(share.Name, share)
-	} else {
-		sharesWaitingOnConfigmaps.Store(share.Name, share)
+		klog.V(4).Infof("AddSharedConfigMap share %s key %s end range", share.Name, key)
 	}
 }
 
@@ -84,18 +75,13 @@ func AddSharedSecret(share *sharev1alpha1.SharedSecret) {
 	br := share.Spec.SecretRef
 	key := BuildKey(br.Namespace, br.Name)
 	klog.V(4).Infof("AddSharedSecret key %s", key)
-	obj, ok := secrets.Load(key)
-	if obj != nil && ok {
-		s := obj.(*corev1.Secret)
-		secretsWithShare.Store(key, s)
+	//obj, ok := secrets.Load(key)
+	s := client.GetSecret(br.Namespace, br.Name)
+	if s != nil {
 		// so this line build a map with a single entry, the share from this event, and then
 		// applies the function(s) supplied by the CSI volume code in order to make changes based
 		// on this event
 		shareSecretsUpdateCallbacks.Range(buildRanger(buildCallbackMap(share.Name, share)))
-		//NOTE we do not store share in shareConfigMaps unless the backing resource is available
-		shareSecrets.Store(share.Name, share)
-	} else {
-		sharesWaitingOnSecrets.Store(share.Name, share)
 	}
 }
 
@@ -103,31 +89,6 @@ func AddSharedSecret(share *sharev1alpha1.SharedSecret) {
 // the registered callbacks to update the content on any volumes using the SharedConfigMap
 func UpdateSharedConfigMap(share *sharev1alpha1.SharedConfigMap) {
 	klog.V(4).Infof("UpdateSharedConfigMap key %s", share.Name)
-	old, ok := shareConfigMaps.Load(share.Name)
-	if !ok || old == nil {
-		AddSharedConfigMap(share)
-		return
-	}
-	oldShare := old.(*sharev1alpha1.SharedConfigMap)
-	diffInstance := false
-	oldBr := oldShare.Spec.ConfigMapRef
-	newBr := share.Spec.ConfigMapRef
-	switch {
-	case oldBr.Namespace != newBr.Namespace:
-		diffInstance = true
-	case oldBr.Name != newBr.Name:
-		diffInstance = true
-	}
-	klog.V(4).Infof("UpdateSharedConfigMap key %s diff %v", share.Name, diffInstance)
-	if !diffInstance {
-		shareConfigMapsUpdateCallbacks.Range(buildRanger(buildCallbackMap(share.Name, share)))
-		return
-	}
-
-	shareConfigMaps.Store(share.Name, share)
-	br := share.Spec.ConfigMapRef
-	key := BuildKey(br.Namespace, br.Name)
-	configmapsWithShares.Delete(key)
 	AddSharedConfigMap(share)
 }
 
@@ -135,31 +96,6 @@ func UpdateSharedConfigMap(share *sharev1alpha1.SharedConfigMap) {
 // the registered callbacks to update the content on any volumes using the SharedSecret
 func UpdateSharedSecret(share *sharev1alpha1.SharedSecret) {
 	klog.V(4).Infof("UpdateSharedSecret key %s", share.Name)
-	old, ok := shareSecrets.Load(share.Name)
-	if !ok || old == nil {
-		AddSharedSecret(share)
-		return
-	}
-	oldShare := old.(*sharev1alpha1.SharedSecret)
-	diffInstance := false
-	oldBr := oldShare.Spec.SecretRef
-	newBr := share.Spec.SecretRef
-	switch {
-	case oldBr.Namespace != newBr.Namespace:
-		diffInstance = true
-	case oldBr.Name != newBr.Name:
-		diffInstance = true
-	}
-	klog.V(4).Infof("UpdateSharedSecret key %s diff %v", share.Name, diffInstance)
-	if !diffInstance {
-		shareSecretsUpdateCallbacks.Range(buildRanger(buildCallbackMap(share.Name, share)))
-		return
-	}
-
-	shareSecrets.Store(share.Name, share)
-	br := share.Spec.SecretRef
-	key := BuildKey(br.Namespace, br.Name)
-	secretsWithShare.Delete(key)
 	AddSharedSecret(share)
 }
 
@@ -169,8 +105,6 @@ func DelSharedConfigMap(share *sharev1alpha1.SharedConfigMap) {
 	br := share.Spec.ConfigMapRef
 	key := BuildKey(br.Namespace, br.Name)
 	klog.V(4).Infof("DelSharedConfigMap key %s", key)
-	configmapsWithShares.Delete(key)
-	shareConfigMaps.Delete(share.Name)
 	shareConfigMapsDeleteCallbacks.Range(buildRanger(buildCallbackMap(share.Name, share)))
 }
 
@@ -180,27 +114,35 @@ func DelSharedSecret(share *sharev1alpha1.SharedSecret) {
 	br := share.Spec.SecretRef
 	key := BuildKey(br.Namespace, br.Name)
 	klog.V(4).Infof("DelSharedSecret key %s", key)
-	secretsWithShare.Delete(key)
-	shareSecrets.Delete(share.Name)
 	shareSecretsDeleteCallbacks.Range(buildRanger(buildCallbackMap(share.Name, share)))
 }
 
 // RegisterSharedConfigMapUpdateCallback will be called as part of the kubelet sending a mount CSI volume request for a pod;
 // then on controller update events for a SharedConfigMap, then function registered here will be called to possibly change
 // storage
-func RegisterSharedConfigMapUpdateCallback(volID string, f func(key, value interface{}) bool) {
+func RegisterSharedConfigMapUpdateCallback(volID, shareID string, f func(key, value interface{}) bool) {
 	shareConfigMapsUpdateCallbacks.Store(volID, f)
 	// cycle through the shareConfigMaps to find the one correlates to this volID's CSI volume mount request; the function
 	// provided then completes the actual storage of the data in the pod
-	shareConfigMaps.Range(f)
+	share := client.GetSharedConfigMap(shareID)
+	if share != nil {
+		f(share.Name, share)
+	} else {
+		klog.Warningf("not found issue using sharedconfigmaps get %s for volume %s", shareID, volID)
+	}
 }
 
 // RegisterSharedSecretUpdateCallback will be called as part of the kubelet sending a mount CSI volume request for a pod;
 // then on controller update events for a SharedSecret, then function registered here will be called to possibly change
 // storage
-func RegisterSharedSecretUpdateCallback(volID string, f func(key, value interface{}) bool) {
+func RegisterSharedSecretUpdateCallback(volID, shareID string, f func(key, value interface{}) bool) {
 	shareSecretsUpdateCallbacks.Store(volID, f)
-	shareSecrets.Range(f)
+	share := client.GetSharedSecret(shareID)
+	if share != nil {
+		f(share.Name, share)
+	} else {
+		klog.Warningf("not found issue using sharedsecrets get %s for volume %s", shareID, volID)
+	}
 }
 
 // UnregisterSharedConfigMapUpdateCallback will be called as part of the kubelet sending a delete CSI volume request for a pod
