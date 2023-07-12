3ca444db19729b97b1fc20ee0ec8653ad3ca4c71
Corey Daley <cdaley@redhat.com>
2023-07-11 22:02:58 -0400

    updating dependencies

--- vendor/github.com/openshift/api/config/v1/types_feature.go
+++ vendor/github.com/openshift/api/config/v1/types_feature.go
@@ -164,25 +164,29 @@ var FeatureSets = map[FeatureSet]*FeatureGateEnabledDisabled{
 	},
 	TechPreviewNoUpgrade: newDefaultFeatures().
 		with(externalCloudProvider).
-		with(externalCloudProviderAzure).
 		with(externalCloudProviderGCP).
+		with(externalCloudProviderExternal).
 		with(csiDriverSharedResource).
 		with(buildCSIVolumes).
 		with(nodeSwap).
 		with(machineAPIProviderOpenStack).
 		with(insightsConfigAPI).
-		with(matchLabelKeysInPodTopologySpread).
 		with(retroactiveDefaultStorageClass).
 		with(pdbUnhealthyPodEvictionPolicy).
 		with(dynamicResourceAllocation).
 		with(admissionWebhookMatchConditions).
+		with(awsSecurityTokenService).
 		with(azureWorkloadIdentity).
 		with(gateGatewayAPI).
 		with(maxUnavailableStatefulSet).
-		with(eventedPleg).
+		without(eventedPleg).
 		with(privateHostedZoneAWS).
 		with(sigstoreImageVerification).
 		with(gcpLabelsTags).
+		with(vSphereStaticIPs).
+		with(routeExternalCertificate).
+		with(automatedEtcdBackup).
+		without(machineAPIOperatorDisableMachineHealthCheckController).
 		toFeatures(defaultFeatures),
 	LatencySensitive: newDefaultFeatures().
 		toFeatures(defaultFeatures),
@@ -191,6 +195,9 @@ var FeatureSets = map[FeatureSet]*FeatureGateEnabledDisabled{
 var defaultFeatures = &FeatureGateEnabledDisabled{
 	Enabled: []FeatureGateDescription{
 		openShiftPodSecurityAdmission,
+		alibabaPlatform, // This is a bug, it should be TechPreviewNoUpgrade. This must be downgraded before 4.14 is shipped.
+		cloudDualStackNodeIPs,
+		externalCloudProviderAzure,
 	},
 	Disabled: []FeatureGateDescription{
 		retroactiveDefaultStorageClass,
